d9aeaede9d00a31382c910e21e14de215d521a47
William <william.miranda@bluesoft.com.br>
2015-08-25 17:34:21 -0300

    creating dsl ffpojoFlatFileReader

--- ffpojo/src/main/java/com/github/ffpojo/FFPojoFlatFileReaderBuilder.java
+++ ffpojo/src/main/java/com/github/ffpojo/FFPojoFlatFileReaderBuilder.java
@@ -2,21 +2,60 @@ package com.github.ffpojo;
 
 import java.io.File;
 import java.io.InputStream;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import com.github.ffpojo.metadata.positional.annotation.FFPojoAccessorType;
 
 public class FFPojoFlatFileReaderBuilder {
 	
-	public FileReaderBuilder file(File file){
-		return null;
+	private File file;
+	private final Set<Class<?>> classes =  new HashSet<Class<?>>();
+	
+	public AddClassBuilder withFile(File file){
+		this.file =  file;
+		return new AddClassBuilderImpl(this);
 	}
 	
-	public InputStreamReaderBuilder inputStream(InputStream inputStream) {
+	public InputStreamReaderBuilder withInputStream(InputStream inputStream) {
 		return null;
 	}
 
-	public void addClass(Class<?> clazz){}
+	private class AddClassBuilderImpl implements AddClassBuilder{
+		private FFPojoFlatFileReaderBuilder builder;
+		
+		public AddClassBuilderImpl(FFPojoFlatFileReaderBuilder builder) {
+			this.builder =  builder;
+		}
+		public FFReaderBuilder addRecordClass(Class<?> clazz) {
+			classes.add(clazz);
+			return new FFReaderBuilderImpl(builder);
+		}
+		public FFReaderBuilder addRecordClasses(List<Class<?>> clazzes) {
+			classes.addAll(clazzes);
+			return new FFReaderBuilderImpl(builder);
+		}
+	}
 	
-	public void addAllClass(List<Class<?>> clazz){ }
+	private class FFReaderBuilderImpl implements FFReaderBuilder{
+		
+		private FFPojoFlatFileReaderBuilder builder;
+
+		public FFReaderBuilderImpl(FFPojoFlatFileReaderBuilder fFPojoFlatFileReaderBuilder){
+			this.builder =  fFPojoFlatFileReaderBuilder;
+		}
+		public List<?> read() {
+			return null;
+		}
+		
+	}
 	
+	public static void main(String[] args) {
+		new FFPojoFlatFileReaderBuilder()
+			.withFile(new File(""))
+			.addRecordClass(FFPojoAccessorType.class)
+			.read();
+	}
 
 }
---
+++ ffpojo/src/main/java/com/github/ffpojo/FFReaderBuilder.java
@@ -0,0 +1,9 @@
+package com.github.ffpojo;
+
+import java.util.List;
+
+public interface FFReaderBuilder {
+
+	public List<?> read ();
+	
+}
